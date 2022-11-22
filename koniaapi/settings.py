c795d5bef97adcb1c12c8320fb3fc6e6c6294ca2
Thiago Henrique Monserrat Rezende <thiagohenriquemonserrat@gmail.com>
2022-11-22 14:13:18 -0300

    Resolver deploy

--- koniaapi/settings.py
+++ koniaapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_on_heroku
 import dotenv
 
 dotenv.load_dotenv()
@@ -130,3 +131,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_on_heroku.settings(locals())
