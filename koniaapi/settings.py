831666fca6bcaf8645b5f1f2a82bc8cfa2b0fc4f
Thiago Henrique Monserrat Rezende <thiagohenriquemonserrat@gmail.com>
2022-11-22 16:53:22 -0300

    Resolver deploy

--- koniaapi/settings.py
+++ koniaapi/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'konia'
 ]
@@ -53,7 +54,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 ROOT_URLCONF = 'koniaapi.urls'
 
